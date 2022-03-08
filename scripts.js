bf01fbaa2c482dd71a28a2052957b848c03bd47f
syncoGitHub <78350570+syncoGitHub@users.noreply.github.com>
2022-03-08 12:35:55 +0100

    Added the first rough elements of the project

---
+++ scripts.js
@@ -0,0 +1,31 @@
+function checkValidAge()
+{
+    console.log("checkValidAge - Start")
+    var age = document.getElementById("form-age")
+
+    if (age.value < 18){
+        alert("You must be at least 18 for apply for this job!");
+        age.value = ""
+        document.getElementById("form-age").focus();
+        
+        return false;
+    }
+    console.log("checkValidAge - End")
+}
+
+function submitSubscription()
+{
+    console.log("submitSubscription - Start")
+    var name    = document.getElementById("form-name").value
+    var surname = document.getElementById("form-surname").value
+    var email   = document.getElementById("form-email").value
+    var age     = document.getElementById("form-age").value
+    var sex     = document.querySelector('input[name="sex-form"]:checked').value
+    
+    var result = "Your name is " + name + " " + surname + ".\<br\>"
+    result += "A " + sex + " of " + age + " years. \<br\>" 
+    result += "The email that you insert is : '" + email + "'."
+
+    document.getElementById("submit-feedback").innerHTML = result
+    console.log("submitSubscription - End")
+}
