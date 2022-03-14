08af48e9f1db2f0ac2a404137c49c1ea2256a25c
syncoGitHub <78350570+syncoGitHub@users.noreply.github.com>
2022-03-14 16:45:20 +0100

    Fixed some IDs and scripts -> Starting CI/CD Pipeline

--- scripts.js
+++ scripts.js
@@ -16,16 +16,22 @@ function checkValidAge()
 function submitSubscription()
 {
     console.log("submitSubscription - Start")
-    var name    = document.getElementById("form-name").value
-    var surname = document.getElementById("form-surname").value
-    var email   = document.getElementById("form-email").value
-    var age     = document.getElementById("form-age").value
-    var sex     = document.querySelector('input[name="sex-form"]:checked').value
-    
-    var result = "Your name is " + name + " " + surname + ".\<br\>"
-    result += "A " + sex + " of " + age + " years. \<br\>" 
-    result += "The email that you insert is : '" + email + "'."
+    try{
+        var name    = document.getElementById("form-name").value
+        var surname = document.getElementById("form-surname").value
+        var email   = document.getElementById("form-email").value
+        var age     = document.getElementById("form-age").value
+        var sex     = document.querySelector('input[name="sex-form"]:checked').value
+        
+        var result = "Your name is " + name + " " + surname + ".\<br\>"
+        result += "A " + sex + " of " + age + " years. \<br\>" 
+        result += "The email that you insert is : '" + email + "'."
 
-    document.getElementById("submit-feedback").innerHTML = result
+        document.getElementById("submit-feedback").innerHTML = result
+    } 
+    catch{
+        console.log("Error during script")
+        document.getElementById("submit-feedback").innerHTML = ""
+    }
     console.log("submitSubscription - End")
 }
